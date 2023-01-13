955e765ba60b22f11f4c64c79a213021ae1f3097
Dipti Pariya <diptipariya@gmail.com>
2023-01-13 18:48:20 +0530

    working on recurring

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,24 @@ const stripe = Stripe("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
 /* GET users listing. */
 router.post("/", async function (req, res, next) {
-  const customer = await stripe.customers.create({
-    email: "name545cb6df-7d02-4426-bcba-cd278a940a50@test.com",
-    address: {
-      city: "Rajkot",
-      country: "IN",
-      line1: "1 Shiva Ganga",
-      state: "GJ",
-    },
-    name: "Test Teksun",
-  });
-
+  const customer = await stripe.customers
+    .create({
+      email: "name545cb6df-7d02-4426-bcba-cd278a940a50@test.com",
+      address: {
+        city: "Rajkot",
+        country: "IN",
+        line1: "1 Shiva Ganga",
+        state: "GJ",
+      },
+      name: "Test Teksun",
+    })
+    .then(async (obj) => {
+      //TODO: plan recurring
+      const subscription = await stripe.subscriptions.create({
+        customer: obj.id,
+        items: [{ price: "price_1MPUKh2eZvKYlo2C7QF1pE1N" }],
+      });
+    });
   res.json({
     status: true,
     data: customer,
